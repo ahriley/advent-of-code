783bbd0055b26a3bdd4073bfdbfd6e02b42ae8f5
ahriley <30327239+ahriley@users.noreply.github.com>
2018-12-01 00:18:39 -0600

    finished day 1

---
+++ src/python/day1.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+with open('data/day1_input.txt') as f:
+    lines = f.read()
+input = lines.split('\n')
+changes = np.array([int(delta) for delta in input[:-1]])
+progress = np.cumsum(changes)
+
+# part 1: end number
+print(progress[-1])
+
+# part 2: first number reached twice
+firstround = len(np.unique(progress)) != len(progress)
+unique = set([]); found = False
+current = progress; currentset = set(progress);
+
+while not found:
+    if unique.intersection(currentset) != set() or firstround:
+        for num in current:
+            numset = set([num])
+            if unique.intersection(numset) != set():
+                result = num
+                found = True
+                break
+            unique = unique.union(numset)
+    unique = unique.union(currentset)
+    current = progress+current[-1]
+    currentset = set(current)
+print(result)
